1b55eff3ebd12a920c5ad92816b1dedde587dd38
Miguel-Pariente <miguelpl32@hotmail.com>
2020-11-16 11:14:40 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,153 @@
+const criptomonedasSelect = document.querySelector("#criptomonedas");
+const monedaSelect = document.querySelector("#moneda");
+const formulario = document.querySelector("#formulario");
+const resultado = document.querySelector("#resultado");
+
+const objBusqueda = {
+  moneda: "",
+  criptomoneda: "",
+};
+
+// Crear Promise
+
+const obtenerCriptomonedas = (criptomonedas) =>
+  new Promise((resolve) => {
+    resolve(criptomonedas);
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  consultarCriptomonedas();
+
+  formulario.addEventListener("submit", submitFormulario);
+
+  criptomonedasSelect.addEventListener("change", leerValor);
+  monedaSelect.addEventListener("change", leerValor);
+});
+
+async function consultarCriptomonedas() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
+
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    const criptomonedas = await obtenerCriptomonedas(resultado.Data);
+    selectCriptomonedas(criptomonedas);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function selectCriptomonedas(criptomonedas) {
+  criptomonedas.forEach((cripto) => {
+    const { FullName, Name } = cripto.CoinInfo;
+
+    const option = document.createElement("option");
+    option.value = Name;
+    option.textContent = FullName;
+    criptomonedasSelect.appendChild(option);
+  });
+}
+
+function leerValor(e) {
+  objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+  e.preventDefault();
+  //Validar
+
+  const { moneda, criptomoneda } = objBusqueda;
+
+  if (moneda === "" || criptomoneda === "") {
+    mostrarAlerta("Ambos campos son obligatorios");
+    return;
+  }
+
+  //Consultar la API con los resultados
+
+  consultarApi();
+}
+
+function mostrarAlerta(msg) {
+  const existeError = document.querySelector(".error");
+
+  if (!existeError) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("error");
+
+    //mensaje de error
+    divMensaje.textContent = msg;
+
+    formulario.appendChild(divMensaje);
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
+
+async function consultarApi() {
+  const { moneda, criptomoneda } = objBusqueda;
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+  mostrarSpinner();
+
+  try {
+    const respuesta = await fetch(url);
+    const cotizacion = await respuesta.json();
+    mostrarCotizacionHTML(cotizacion.DISPLAY[criptomoneda][moneda]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function mostrarCotizacionHTML(cotizacion) {
+  limpiarHTML();
+
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE } = cotizacion;
+
+  const precio = document.createElement("p");
+  precio.classList.add("precio");
+  precio.innerHTML = `El precio es: <span>${PRICE}</span>`;
+
+  const precioAlto = document.createElement("p");
+  precioAlto.innerHTML = `<p> Precio más alto del dia <span>${HIGHDAY}</span> </p>`;
+
+  const precioBajo = document.createElement("p");
+  precioBajo.innerHTML = `<p> Precio más bajo del dia <span>${LOWDAY}</span> </p>`;
+
+  const ultimasHoras = document.createElement("p");
+  ultimasHoras.innerHTML = `<p> Variación últimas 24 horas <span>${CHANGEPCT24HOUR}%</span> </p>`;
+
+  const ultimasActualizacion = document.createElement("p");
+  ultimasActualizacion.innerHTML = `<p> Última Actualización <span>${LASTUPDATE}</span> </p>`;
+
+  resultado.appendChild(precio);
+  resultado.appendChild(precioAlto);
+  resultado.appendChild(precioBajo);
+  resultado.appendChild(ultimasHoras);
+  resultado.appendChild(ultimasActualizacion);
+}
+
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+function mostrarSpinner() {
+  limpiarHTML();
+
+  const spinner = document.createElement("div");
+  spinner.classList.add("spinner");
+
+  spinner.innerHTML = `
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+  
+  `;
+
+  resultado.appendChild(spinner);
+}
